89e2b885fd5c28f8eda685d12b64b4275bd28e27
Romina Pineda <romi.macka@gmail.com>
2020-08-26 03:16:06 -0400

    add round(2)

--- escape.rb
+++ escape.rb
@@ -1,4 +1,8 @@
-g = ARGV[0].to_f
-r = ARGV[1].to_f
-
-puts Math.sqrt(2*g*r)
+if ARGV[1].nil?
+    puts "Falta ingresar un valor"
+else
+    g = ARGV[0].to_f
+    r = ARGV[1].to_f
+    velocidad = Math.sqrt(2*g*r)
+    puts "La velocidad de escape del planeta es de #{velocidad.round(2)} mt/seg"
+end
